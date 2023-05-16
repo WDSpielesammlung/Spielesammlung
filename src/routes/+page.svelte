5d396d9e931d38566cd87aeef5accaf76f9f525d
Nils Abels <nils.abels23@gmail.com>
2023-05-16 16:21:03 +0200

    svelstrap example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+    import 'bootstrap/dist/css/bootstrap.min.css';
+    import { Button } from 'sveltestrap';
+    const color = 'primary';
+</script>
+
+
 <h1>Paul stinkt nach Arschkriechen</h1>
+<Button outline {color}>sveltestrap button test</Button>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
