a70d3d64bbbfcdd9b71bc7f4101e4092f2aca041
Nils Abels <nils.abels23@gmail.com>
2023-05-30 13:00:36 +0200

    flappy bird and spaceinvader endpoints

--- src/routes/api/SpaceInvaders/+server.ts
+++ src/routes/api/SpaceInvaders/+server.ts
@@ -1,5 +1,6 @@
 import { error, json } from '@sveltejs/kit';
 import { db } from '$lib/database';
+//save new highscore for current user
 export async function POST({ request, locals }) {
 	if (!locals.user) {
 		throw error(401, { message: 'User not logged in' });
@@ -11,21 +12,38 @@ export async function POST({ request, locals }) {
 
 	if (highscore) {
 		if (highscore.score < body.score) {
-			await db.spaceinvader.update({
-				where: { id: highscore.id },
-				data: { score: body.score }
-			});
-			return json({ message: 'new highscore added' }, { status: 201 });
+			try {
+				await db.spaceinvader.update({
+					where: { id: highscore.id },
+					data: { score: body.score }
+				});
+				return json({ message: 'new highscore added' }, { status: 201 });
+			} catch (error) {
+				console.log('database connection failed \n' + error);
+			}
 		} else {
 			return json({ message: 'old highscore greater than current sore' }, { status: 200 });
 		}
 	} else {
-		await db.spaceinvader.create({
-			data: {
-				score: body.score,
-				userId: locals.user.id
-			}
-		});
-		return json({ message: 'new highscore added' }, { status: 201 });
+		try {
+			await db.spaceinvader.create({
+				data: {
+					score: body.score,
+					userId: locals.user.id
+				}
+			});
+			return json({ message: 'new highscore added' }, { status: 201 });
+		} catch (error) {
+			console.log('database connection failed \n' + error);
+		}
+	}
+}
+//get all user highscores
+export async function GET() {
+	try {
+		const highscores = await db.spaceinvader.findMany({});
+		return json(highscores, { status: 200 });
+	} catch (error) {
+		console.log('database connection failed \n' + error);
 	}
 }
