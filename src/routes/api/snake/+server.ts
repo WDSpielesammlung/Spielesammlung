b9cc3ad3f3cf0ae69e037a996ecad8581b0d4c02
Nils Abels <nils.abels23@gmail.com>
2023-05-30 14:02:21 +0200

    all Highscore endpoints finished

---
+++ src/routes/api/snake/+server.ts
@@ -0,0 +1,49 @@
+import { error, json } from '@sveltejs/kit';
+import { db } from '$lib/database';
+//save new highscore for current user
+export async function POST({ request, locals }) {
+	if (!locals.user) {
+		throw error(401, { message: 'User not logged in' });
+	}
+	const body = await request.json();
+	const highscore = await db.snake.findUnique({
+		where: { userId: locals.user.id }
+	});
+
+	if (highscore) {
+		if (highscore.score < body.score) {
+			try {
+				await db.snake.update({
+					where: { id: highscore.id },
+					data: { score: body.score }
+				});
+				return json({ message: 'new highscore added' }, { status: 201 });
+			} catch (error) {
+				console.log('database connection failed \n' + error);
+			}
+		} else {
+			return json({ message: 'old highscore greater than current sore' }, { status: 200 });
+		}
+	} else {
+		try {
+			await db.snake.create({
+				data: {
+					score: body.score,
+					userId: locals.user.id
+				}
+			});
+			return json({ message: 'new highscore added' }, { status: 201 });
+		} catch (error) {
+			console.log('database connection failed \n' + error);
+		}
+	}
+}
+//get all user highscores
+export async function GET() {
+	try {
+		const highscores = await db.snake.findMany({});
+		return json(highscores, { status: 200 });
+	} catch (error) {
+		console.log('database connection failed \n' + error);
+	}
+}
