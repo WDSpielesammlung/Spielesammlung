5000c3e23aba97284eec8587c2df8f0610a96870
Nils Abels <nils.abels23@gmail.com>
2023-05-30 12:07:47 +0200

    SpaceInvaders Highscore

--- src/routes/api/SpaceInvaders/+server.ts
+++ src/routes/api/SpaceInvaders/+server.ts
@@ -1,7 +1,31 @@
-import { fail, json } from '@sveltejs/kit';
-export function GET({ request, locals }) {
+import { error, json } from '@sveltejs/kit';
+import { db } from '$lib/database';
+export async function POST({ request, locals }) {
 	if (!locals.user) {
-		return fail(401, { message: 'User not logged in' });
+		throw error(401, { message: 'User not logged in' });
 	}
-	return json({ userid: locals.user.id }, { status: 201 });
+	const body = await request.json();
+	const highscore = await db.spaceinvader.findUnique({
+		where: { userId: locals.user.id }
+	});
+
+	if (highscore) {
+		if (highscore.score < body.score) {
+			await db.spaceinvader.update({
+				where: { id: highscore.id },
+				data: { score: body.score }
+			});
+			return json({ message: 'new highscore added' }, { status: 201 });
+		} else {
+			return json({ message: 'old highscore greater than current sore' }, { status: 200 });
+		}
+	} else {
+		await db.spaceinvader.create({
+			data: {
+				score: body.score,
+				userId: locals.user.id
+			}
+		});
+		return json({ message: 'new highscore added' }, { status: 201 });
+	}
 }
