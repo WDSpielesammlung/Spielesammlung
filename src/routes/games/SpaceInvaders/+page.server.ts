d27692c017551385f49c6adc988da3a3b9eca21f
Nils Abels <nils.abels23@gmail.com>
2023-06-02 12:34:35 +0200

    API_URL + SpaceInvader Highscore

--- src/routes/games/SpaceInvaders/+page.server.ts
+++ src/routes/games/SpaceInvaders/+page.server.ts
@@ -1,15 +1,17 @@
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export async function load({ locals }) {
+	if (!locals.user) {
+		throw redirect(302, '/login');
+	}
+	const url1 = process.env.API_URL + 'spaceInvaders/user?userId=' + locals.user.id;
+	const url2 = process.env.API_URL + 'spaceInvaders';
 	try {
-		const userHighscoreResponse = await fetch(
-			'http://localhost:5173/api/spaceInvaders/user?userId=' + locals.user.id
-		);
+		const userHighscoreResponse = await fetch(url1);
 		const userHighscoreData = await userHighscoreResponse.json();
-
-		const allHighscoresResponse = await fetch('http://localhost:5173/api/spaceInvaders');
+		const allHighscoresResponse = await fetch(url2);
 		const allHighscoresData = await allHighscoresResponse.json();
-
 		return { userHighscoreData, allHighscoresData };
 	} catch (err) {
 		console.log(err);
