8ed31283bb531848894decca51de66e3322486f8
Nils Abels <nils.abels23@gmail.com>
2023-06-13 09:07:04 +0200

    type error

--- src/routes/games/flappyBird/+page.server.ts
+++ src/routes/games/flappyBird/+page.server.ts
@@ -1,7 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export async function load({ locals }) {
+export const load = async ({ locals }) => {
 	if (!locals.user) {
 		throw redirect(302, '/login');
 	}
@@ -10,9 +10,8 @@ export async function load({ locals }) {
 	const url3 = process.env.API_URL + '/flappyBird/user?userId=' + locals.user.id + '&difficulty=1';
 	const url4 = process.env.API_URL + '/flappyBird?difficulty=1';
 	const url5 = process.env.API_URL + '/flappyBird/user?userId=' + locals.user.id + '&difficulty=0';
-	const url6 = process.env.API_URL + '/flappyBird?difficulty=0'
+	const url6 = process.env.API_URL + '/flappyBird?difficulty=0';
 	try {
-		
 		const userHighscoreResponse2 = await fetch(url1);
 		const userHighscoreData2 = await userHighscoreResponse2.json();
 		const allHighscoresResponse2 = await fetch(url2);
@@ -25,37 +24,31 @@ export async function load({ locals }) {
 		const userHighscoreData0 = await userHighscoreResponse0.json();
 		const allHighscoresResponse0 = await fetch(url6);
 		const allHighscoresData0 = await allHighscoresResponse0.json();
-		
-		const user = new Array(0,0,0)
-		const overall = new Array(0,0,0)
 
-		if(userHighscoreData0)
-		{
-			user[0]=userHighscoreData0[0].score
+		const user = new Array(0, 0, 0);
+		const overall = new Array(0, 0, 0);
+
+		if (userHighscoreData0) {
+			user[0] = userHighscoreData0[0].score;
 		}
-		if(userHighscoreData1)
-		{
-			user[1]=userHighscoreData1[0].score
+		if (userHighscoreData1) {
+			user[1] = userHighscoreData1[0].score;
 		}
-		if(userHighscoreData2)
-		{
-			user[2]=userHighscoreData2[0].score
+		if (userHighscoreData2) {
+			user[2] = userHighscoreData2[0].score;
 		}
-		if(allHighscoresData0)
-		{
-			overall[0]=allHighscoresData0[0].score
+		if (allHighscoresData0) {
+			overall[0] = allHighscoresData0[0].score;
 		}
-		if(allHighscoresData1)
-		{
-			overall[1]=allHighscoresData1[0].score
+		if (allHighscoresData1) {
+			overall[1] = allHighscoresData1[0].score;
 		}
-		if(allHighscoresData2)
-		{
-			overall[2]=allHighscoresData2[0].score
+		if (allHighscoresData2) {
+			overall[2] = allHighscoresData2[0].score;
 		}
-		
+
 		return { user, overall };
 	} catch (err) {
 		console.log(err);
 	}
-}
+};
