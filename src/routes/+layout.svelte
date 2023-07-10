d0a49b68135fbd708ca84818547c27d0233b9711
jonbraun <jonbraun@rhrk.uni-kl.de>
2023-07-11 01:59:33 +0200

    landingpage object bug | navbar fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -292,7 +292,7 @@
 		.navbar {
 			top: 0;
 			width: 5rem;
-			height: 100vh;
+			height: 100%;
 		}
 
 		.navbar:hover {
@@ -301,6 +301,7 @@
 		.navbar:hover + .mainContent {
 			margin-left: 16rem;
 			transition: margin-left var(--transition-speed) ease;
+			height: 100%;
 		}
 
 		.navbar:hover .link-text {
